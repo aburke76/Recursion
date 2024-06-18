e2b6246e04d6dc4a0b4c704510a56dce791a3143
aburke76 <alexlburke8771@gmail.com>
2024-06-18 16:47:26 -0400

    recursion done

--- fib.js
+++ fib.js
@@ -1,13 +1,15 @@
 function fibs(n) {
     const array = [0, 1];
     if (n <= 0) {
-        console.log("Enter number greater than 0");
-    } else if (n == 1) {
-        console.log(array[0]);
-    } else if (n == 2) {
-        console.log(array[1]);
+        return "Enter number greater than 0";
+    } else if (n === 0) {
+        return [];
+    } else if (n === 1) {
+        return array[0];
+    } else if (n === 2) {
+        return array[1];
     } else {
-        for (let i = 1; i < n; i++) {
+        for (let i = 2; i < n; i++) {
             const nextNum = array[array.length - 1] + array[array.length - 2];
             array.push(nextNum);
         }
@@ -15,6 +17,20 @@ function fibs(n) {
     return array;
 }
 
-function fibsRec(n) {}
+function fibsRec(n, arr = [0, 1]) {
+    if (n < 0) {
+        return "Enter number greater than 0";
+    } else if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return [0];
+    } else if (n === 2) {
+        return [0, 1];
+    } else {
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+        return fibsRec(n, arr);
+    }
+}
 
 console.log(`This is iteration: ${fibs(8)}`);
+console.log(`This is recursion: ${fibsRec(8)}`);
